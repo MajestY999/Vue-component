a491049fe5c8ce745c0f94cf64f2d5bace572a9b
MajestY999 <Vilisovartem@gmail.com>
2026-06-23 22:06:39 +1000

    Задание 3

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,21 +1,29 @@
 <template>
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.css">
+  <div
+    id="app"
+    class="ui container"
+    style="padding-top: 40px; font-family: sans-serif"
+  >
+    <h1>Notification Center</h1>
 
-  <div id="app" class="ui container" style="margin-top: 20px;">
-    <h1>GitHub Profiles</h1>
-    
-    <GithubUserCard 
-      v-for="u in usernames" 
-      :key="u" 
-      :username="u" 
-    />
+    <NotificationMessage type="info">
+      <template #header>Вы имеете право на вознаграждение</template>
+      Перейдите на страницу <b>специальных предложений</b>, чтобы увидеть его
+      прямо сейчас.
+    </NotificationMessage>
+
+    <NotificationMessage type="success">
+      <template #header>Аккаунт успешно создан!</template>
+      Добро пожаловать на платформу. Проверьте вашу почту для подтверждения.
+    </NotificationMessage>
+
+    <NotificationMessage type="error">
+      <template #header>Произошла ошибка</template>
+      Не удалось отправить форму. Попробуйте перезагрузить страницу.
+    </NotificationMessage>
   </div>
 </template>
 
 <script setup>
-import { ref } from 'vue'
-import GithubUserCard from './components/GithubUserCard.vue'
-
-
-const usernames = ref(['danielkellyio', 'hootlex', 'rahaug', 'akryum'])
+import NotificationMessage from "./components/NotificationMessage.vue";
 </script>
---
+++ vue-project/src/components/NotificationMessage.vue
@@ -0,0 +1,46 @@
+<template>
+  <link
+    rel="stylesheet"
+    href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.css"
+  />
+
+  <div v-if="isVisible" :class="['ui', type, 'message']">
+    <i class="close icon" @click="closeNotification"></i>
+
+    <div class="header">
+      <slot name="header"></slot>
+    </div>
+
+    <p>
+      <slot></slot>
+    </p>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+defineProps({
+  type: {
+    type: String,
+    default: "info",
+  },
+});
+
+const isVisible = ref(true);
+
+const closeNotification = () => {
+  isVisible.value = false;
+};
+</script>
+
+<style scoped>
+.ui.message .close.icon {
+  cursor: pointer;
+  opacity: 0.7;
+  transition: opacity 0.2s;
+}
+.ui.message .close.icon:hover {
+  opacity: 1;
+}
+</style>
