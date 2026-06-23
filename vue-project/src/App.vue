df2c625d556fd73d8867c8e2006bb29f4cad7a9c
MajestY999 <Vilisovartem@gmail.com>
2026-06-23 21:17:54 +1000

    Задание 1

---
+++ vue-project/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app" style="text-align: center; padding-top: 40px; font-family: sans-serif;">
+    <h1>Изучение слотов во Vue.js</h1>
+
+    <BaseAlert>
+      Это первое уведомление, здесь просто текст.
+    </BaseAlert>
+
+    <BaseAlert>
+      <b>Внимание!</b> Это очень важное и  жирное сообщение.
+    </BaseAlert>
+
+    <BaseAlert>
+      Тут какая-то ошибка. Подробнее можно узнать <a href="#">здесь</a>.
+    </BaseAlert>
+  </div>
+</template>
+
+<script setup>
+import BaseAlert from './components/BaseAlert.vue'
+</script>
---
+++ vue-project/src/components/BaseAlert.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="alert-box">
+    <span class="alert-icon">📢</span>
+    <div class="alert-content">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.alert-box {
+  display: flex;
+  align-items: center;
+  border: 1px solid #333;
+  padding: 15px;
+  margin: 10px auto;
+  max-width: 400px;
+  background-color: #f9f9f9;
+  border-radius: 4px;
+  text-align: left;
+}
+.alert-icon {
+  font-size: 20px;
+  margin-right: 15px;
+}
+.alert-content {
+  font-size: 14px;
+}
+</style>
